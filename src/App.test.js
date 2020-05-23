82bd2b93531a3b1024970d02befb47a91fa361bb
KANIKA SHRIVASTAV <kanika.shrivastav@quantcommune.com>
2020-05-24 00:17:49 +0530

    [Kanika]Add. Test for checking whether planets fetch have been called.

--- src/App.test.js
+++ src/App.test.js
@@ -5,24 +5,40 @@ import App from './App';
 import { fetchPlanets, fetchToken } from './services'
 
 jest.mock('./services.js')
-const token = 'sfgasdfdgdfg87afa868adfa'
-const planets = [{ name: 'Planet 1' }, { name: 'Planet 2' }, { name: 'Planet 3' }, { name: 'Planet 4' }]
 
 
-fetchToken.mockResolvedValue({ token })
-fetchPlanets.mockResolvedValue(planets)
+describe('<App />', () => {
+  beforeEach(() => {
 
-test('renders learn react link',async () => {
-  const { asFragment, getAllByTestId} = render(<App />);
+    const token = 'sfgasdfdgdfg87afa868adfa'
+    const planets = [
+      { name: 'Planet 1' }, 
+      { name: 'Planet 2' }, 
+      { name: 'Planet 3' }, 
+      { name: 'Planet 4' }
+    ]
+    fetchToken.mockResolvedValue({ token })
+    fetchPlanets.mockResolvedValue(planets)
+  })
+  afterEach(() => {
+    fetchToken.mockClear();
+    fetchPlanets.mockClear();
+  })
+  test('should show the planets select in the UI', async () => {
+    const { asFragment, getAllByTestId } = render(<App />);
     user.click(getAllByTestId("planet-select")[0])
     user.click(getAllByTestId("planet-select")[1])
     user.click(getAllByTestId("planet-select")[2])
     user.click(getAllByTestId("planet-select")[3])
     await waitForDomChange()
-  expect(asFragment()).toMatchSnapshot()
-});
+    expect(asFragment()).toMatchSnapshot()
+  });
 
-
-test('should call token on load.', () => {
-
+  test('should call token on load', async () => {
+    const { rerender } = render(<App />);
+    await waitForDomChange()
+    expect(fetchToken).toHaveBeenCalledTimes(1)
+    rerender(<App />)
+    expect(fetchToken).toHaveBeenCalledTimes(1)
+  });
 });
