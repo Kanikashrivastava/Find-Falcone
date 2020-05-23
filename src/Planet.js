8d20ce5af4144830b961593a03151cea878505fe
KANIKA SHRIVASTAV <kanika.shrivastav@quantcommune.com>
2020-05-24 03:12:59 +0530

    [Kanika]Add. text casees added for vehicles selector and planet selector.

--- src/Planet.js
+++ src/Planet.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import FormControl from "@material-ui/core/FormControl";
 import Radio from "@material-ui/core/Radio";
@@ -19,13 +19,23 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export function DestinationVehicleBook({ optionNum, planets, vehicles }) {
+export function DestinationVehicleBook({ optionNum, planets, vehicles, handlePlanetChange, handleVehicleChange }) {
   const classes = useStyles();
   const [selectedPlanet, setSelectedPlanet] = useState("default");
   const [selectedVechile, setSelectedVechile] = useState("default");
   const isPlanetSelected = () => {
     return selectedPlanet && selectedPlanet !== "default";
   };
+  const onPlanetChange = (e) => {
+    const { value } = e.target;
+    setSelectedPlanet(value)
+    handlePlanetChange(value);
+  }
+  const onVehicleChange = (e) => {
+    const { value } = e.target;
+    setSelectedVechile(value)
+    handleVehicleChange(value);
+  }
   return (
     <>
       <FormControl variant="filled" className={classes.formControl}>
@@ -34,16 +44,16 @@ export function DestinationVehicleBook({ optionNum, planets, vehicles }) {
           id="planet-select"
           data-testId="planet-select"
           className={classes.select}
-          onChange={(e) => setSelectedPlanet(e.target.value)}
+          onChange={onPlanetChange}
         >
-          {[{name: "default", distance: 0}, ...planets].map((planet) => {
+          {[{ name: "default", distance: 0 }, ...planets].map((planet) => {
             return (
               <option
                 data-testId={planet.name}
                 value={planet.name}
                 selected={selectedPlanet === planet.name}
               >
-                {planet.name === 'default'? 'Select Planet': planet.name}
+                {planet.name === 'default' ? 'Select Planet' : planet.name}
               </option>
             );
           })}
@@ -53,13 +63,13 @@ export function DestinationVehicleBook({ optionNum, planets, vehicles }) {
             aria-label="vehicles"
             name="vehicles"
             value={selectedVechile}
-            onChange={(e) => setSelectedVechile(e.target.value)}
+            onChange={onVehicleChange}
           >
             {vehicles.map((vehicle) => {
               return (
                 <FormControlLabel
                   value={vehicle.name}
-                  control={<Radio />}
+                  control={<Radio data-testId={vehicle.name}/>}
                   label={`${vehicle.name.toUpperCase()} (${vehicle.total_no})`}
                 />
               );
@@ -69,4 +79,9 @@ export function DestinationVehicleBook({ optionNum, planets, vehicles }) {
       </FormControl>
     </>
   );
+}
+
+DestinationVehicleBook.defaultProps = {
+  handlePlanetChange: () => { },
+  handleVehicleChange: () => { }
 }
--- src/Planet.test.js
+++ src/Planet.test.js
@@ -21,3 +21,39 @@ test('renders planets options', async () => {
     user.selectOptions(screen.getByTestId(/planet-select/), [props.planets[0].name])
     expect(asFragment()).toMatchSnapshot()
 });
+
+test(`should trigger handle planet change when user selects an option`, async () => {
+    const props = {
+        optionNum: 1,
+        planets: [{ name: 'Planet 1' }, { name: 'Planet 2' }],
+        vehicles: [
+            { name: 'Vehicles 1', total_no: 1 },
+            { name: 'Vehicles 2', total_no: 2 }
+        ],
+        handlePlanetChange: jest.fn()
+    }
+    const utils = render(<DestinationVehicleBook {...props} />);
+    user.click(utils.getByTestId("planet-select"))
+    user.selectOptions(utils.getByTestId(/planet-select/), [props.planets[0].name])
+    expect(props.handlePlanetChange).toHaveBeenCalledTimes(1)
+
+})
+
+test(`should trigger handle vehicle change when user selects a vehicle`, async () => {
+    const props = {
+        optionNum: 1,
+        planets: [{ name: 'Planet 1' }, { name: 'Planet 2' }],
+        vehicles: [
+            { name: 'Vehicles 1', total_no: 1 },
+            { name: 'Vehicles 2', total_no: 2 }
+        ],
+        handleVehicleChange: jest.fn()
+    }
+    const utils = render(<DestinationVehicleBook {...props} />);
+    user.click(utils.getByTestId("planet-select"))
+    user.selectOptions(utils.getByTestId(/planet-select/), [props.planets[0].name])
+    user.selectOptions(utils.getByTestId(/Vehicles 1/), [props.vehicles[0].name])
+    // console.log(prettyDOM(utils.getByLabelText(/vehicles 2/)),)
+    expect(props.handleVehicleChange).toHaveBeenCalledTimes(1)
+
+})
